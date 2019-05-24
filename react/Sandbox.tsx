386b2df0320aa558a0fa3cbe6ba6f01f0d66c93b
Guilherme Rodrigues <gadr90@gmail.com>
2019-05-23 23:54:08 -0300

    Rewrite as class component to get more predictable re-rendering behaviour and make clear initialContent is only inserted once

--- react/Sandbox.tsx
+++ react/Sandbox.tsx
@@ -1,15 +1,26 @@
-import React, { useEffect, useRef, RefObject } from 'react'
+import React, { RefObject, PureComponent, createRef } from 'react'
 import { NoSSR, canUseDOM } from 'vtex.render-runtime'
 import stringify from 'safe-json-stringify'
 
 interface StyleContainer {
   href?: string
-  rules?: string[]
+  rules?: string
 }
 
 interface IframeOptions {
-  allowCookies: boolean,
-  cookieEventType: string,
+  cookieEventType: string
+  cookie: string
+  styles: StyleContainer[]
+}
+
+interface Props {
+  initialContent?: string
+  width?: string
+  height?: string
+  allowCookies?: boolean
+  allowStyles?: boolean
+  iframeRef?: RefObject<HTMLIFrameElement>
+  hidden?: boolean
 }
 
 // Create objects representing styles, either with href or the actual rules, to be passed to the iframe.
@@ -21,7 +32,7 @@ function getExistingStyles() {
       if (stylesheet.href) {
         styles.push({href: stylesheet.href})
       } else {
-        styles.push({rules: Array.prototype.map.call(stylesheet.rules, (r: any) => r.cssText) as string[] })
+        styles.push({rules: Array.prototype.map.call(stylesheet.rules, (r: any) => r.cssText).join('\n') as string })
       }
     }
   }
@@ -39,10 +50,12 @@ if (canUseDOM) {
   })
 }
 
-function init (options: IframeOptions) {
-  let contentInitialized = false
+function initIframe (options: IframeOptions) {
+  const {styles, cookie, cookieEventType} = options
 
-  if (options.allowCookies) {
+  // Mount cookie
+  if (cookie) {
+    (window as any).__cookie = cookie
     Object.defineProperty(document, 'cookie', {
       get: () => (window as any).__cookie,
       set: (value: string) => {
@@ -51,120 +64,131 @@ function init (options: IframeOptions) {
         }
         const [clean] = value.split(';');
         (window as any).__cookie = `${clean}; ` + (window as any).__cookie
-        window.parent.postMessage({type: options.cookieEventType, value}, '*')
+        window.parent.postMessage({type: cookieEventType, value}, '*')
       },
     })
   }
 
+  // Add styles
+  if (styles && Array.isArray(styles)) {
+    styles.forEach((style: StyleContainer) => {
+      if (style.href) {
+        const link = document.createElement('link')
+        link.href = style.href
+        link.type = 'text/css'
+        link.rel = 'stylesheet'
+        document.head.appendChild(link)
+      } else if (style.rules) {
+        const sheet = document.createElement('style')
+        sheet.innerHTML = style.rules
+        document.head.appendChild(sheet)
+      }
+    })
+  }
+
   window.addEventListener('message', function(event: MessageEvent) {
-    const {styles, props, content, cookie} = event.data
+    const {props} = event.data
     // Add props to window
     if (props) {
       (window as any).props = JSON.parse(props)
     }
-    // Mount cookie
-    if (options.allowCookies && cookie) {
-      (window as any).__cookie = cookie
-    }
-    // Mount block content
-    if (content && !contentInitialized) {
-      contentInitialized = true
-      var range = document.createRange()
-      range.setStart(document.body, 0)
-      document.body.appendChild(range.createContextualFragment(content))
-    }
-    // Add styles
-    if (styles && Array.isArray(styles)) {
-      styles.forEach((style: StyleContainer) => {
-        if (style.href) {
-          const link = document.createElement('link')
-          link.href = style.href
-          link.type = 'text/css'
-          link.rel = 'stylesheet'
-          document.head.appendChild(link)
-        } else if (style.rules) {
-          const sheet = document.createElement('style')
-          sheet.innerHTML = style.rules.join('\n')
-          document.head.appendChild(sheet)
-        }
-      })
-    }
   })
 }
 
-const Sandbox: StorefrontFunctionComponent<SandboxProps> = ({ content, width = '100%', height, allowCookies, allowStyles, iframeRef, hidden, ...props }) => {
-  delete (props as any).runtime
-  const injected = encodeURIComponent(`<script>${init.toString()};init(${stringify({allowCookies, cookieEventType: type})});</script>`)
-  const ref = iframeRef || useRef<HTMLIFrameElement>(null)
+export default class Sandbox extends PureComponent<Props> {
+  public iframeRef: RefObject<HTMLIFrameElement>
 
-  if (allowStyles === undefined) {
-    allowStyles = true
+  public schema = {
+    title: 'editor.sandbox.title',
+    description: 'editor.sandbox.description',
+    type: 'object',
+    properties: {
+      width: {
+        title: 'editor.sandbox.width.title',
+        description: 'editor.sandbox.width.description',
+        type: 'string',
+        default: null,
+      },
+      height: {
+        title: 'editor.sandbox.height.title',
+        description: 'editor.sandbox.height.description',
+        type: 'string',
+        default: null,
+      },
+      initialContent: {
+        title: 'editor.sandbox.initialContent.title',
+        description: 'editor.sandbox.initialContent.description',
+        type: 'string',
+        default: null,
+      },
+      allowCookies: {
+        title: 'editor.sandbox.allowCookies.title',
+        description: 'editor.sandbox.allowCookies.description',
+        type: 'boolean',
+        default: false,
+      },
+    },
   }
 
-  useEffect(() => {
-    if (ref.current && ref.current.contentWindow) {
-      const styles = allowStyles && getExistingStyles()
-      const safeProps = stringify(props)
-      const cookie = allowCookies && document.cookie
-      ref.current.contentWindow.postMessage({props: safeProps, content, styles, cookie}, '*')
+  private injectedDocument: string = ''
+
+  public constructor(props: Props) {
+    super(props)
+    
+    const { initialContent, allowCookies, allowStyles: allowStylesProp } = props
+    const allowStyles = allowStylesProp === undefined ? true : allowStylesProp
+    this.iframeRef = props.iframeRef || createRef<HTMLIFrameElement>()
+    
+    if (!canUseDOM) {
+      return
     }
-  })
+    
+    const cookie = allowCookies && document.cookie
+    const styles = allowStyles && getExistingStyles()
+    this.injectedDocument = `<html><head><script>window.props=${
+      this.safeProps
+    };(${
+      initIframe.toString()
+    })(${
+      stringify({cookieEventType: type, styles, cookie})
+    });</script></head><body>${initialContent}</body></html>`
+  }
 
-  return (
-    <NoSSR>
-      <iframe
-        ref={ref}
-        frameBorder={0}
-        style={hidden ? {display: 'none'} : {width, height}}
-        sandbox="allow-scripts"
-        className="vtex-sandbox-iframe"
-        hidden={hidden}
-        src={"data:text/html,"+injected}>
-      </iframe>
-    </NoSSR>
-  )
+  public componentDidMount() {
+    this.updateIframe()
+  }
+
+  public componentDidUpdate() {
+    this.updateIframe()
+  }
+
+  public render() {
+    const { width = '100%', height, hidden } = this.props
+    return (
+      <NoSSR>
+        <iframe
+          ref={this.iframeRef}
+          frameBorder={0}
+          style={hidden ? {display: 'none'} : {width, height}}
+          sandbox="allow-scripts"
+          className="vtex-sandbox-iframe"
+          hidden={hidden}
+          src={`data:text/html,${encodeURIComponent(this.injectedDocument)}`}>
+        </iframe>
+      </NoSSR>
+    )
+  }
+
+  private get safeProps () {
+    const {runtime, iframeRef, initialContent, allowCookies, height, width, treePath, ...rest} = this.props as any
+    return stringify(rest)
+  }
+
+  private updateIframe() {
+    const ref = this.iframeRef
+    if (ref.current && ref.current.contentWindow) {
+      const props = this.safeProps
+      ref.current.contentWindow.postMessage({props}, '*')
+    }
+  }
 }
-
-interface SandboxProps {
-  content?: string
-  width?: string
-  height?: string
-  allowCookies?: boolean
-  allowStyles?: boolean
-  iframeRef?: RefObject<HTMLIFrameElement>
-  hidden?: boolean
-}
-
-Sandbox.schema = {
-  title: 'editor.sandbox.title',
-  description: 'editor.sandbox.description',
-  type: 'object',
-  properties: {
-    width: {
-      title: 'editor.sandbox.width.title',
-      description: 'editor.sandbox.width.description',
-      type: 'string',
-      default: null,
-    },
-    height: {
-      title: 'editor.sandbox.height.title',
-      description: 'editor.sandbox.height.description',
-      type: 'string',
-      default: null,
-    },
-    content: {
-      title: 'editor.sandbox.content.title',
-      description: 'editor.sandbox.content.description',
-      type: 'string',
-      default: null,
-    },
-    allowCookies: {
-      title: 'editor.sandbox.allowCookies.title',
-      description: 'editor.sandbox.allowCookies.description',
-      type: 'boolean',
-      default: false,
-    },
-  },
-}
-
-export default Sandbox
