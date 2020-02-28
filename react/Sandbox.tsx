5fea3b6edaaa107fed7830d302e22e8a6edf2398
Arthur Cunha <arthur.cunha@cupula.local>
2020-02-28 16:11:35 -0300

    add charset=UTF-8

--- react/Sandbox.tsx
+++ react/Sandbox.tsx
@@ -150,7 +150,7 @@ class Sandbox extends Component<Props> {
           sandbox="allow-scripts allow-forms allow-popups allow-popups-to-escape-sandbox"
           className="vtex-sandbox-iframe"
           hidden={hidden}
-          src={`data:text/html,${encodeURIComponent(this.injectedDocument)}`}>
+          src={`data:text/html;charset=UTF-8,${encodeURIComponent(this.injectedDocument)}`}>
         </iframe>
       </NoSSR>
     )
